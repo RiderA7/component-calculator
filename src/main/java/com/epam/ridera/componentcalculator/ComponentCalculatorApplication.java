09859bb7f95d2ade7b2cee96e68802a87850d2ba
ridera7 <ridera7@gmail.com>
2024-08-30 13:07:06 +0300

    Initial commit

---
+++ src/main/java/com/epam/ridera/componentcalculator/ComponentCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.epam.ridera.componentcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponentCalculatorApplication.class, args);
+    }
+
+}
